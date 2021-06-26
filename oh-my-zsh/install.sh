341e8c3ded633884a3557fa4927466919faded91
Johannes Schnatterer <johannes@schnatterer.info>
2021-06-26 11:32:55 +0200

    Introduce default version of oh-my-zsh.
    templates/zshrc.zsh-template at ohmyzsh/ohmyzsh@706b2f3

---
+++ oh-my-zsh/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if ! [ -d "${HOME}/.oh-my-zsh" ]; then
+  echo "Installing oh-my-zsh repo to $HOME/.oh-my-zsh"
+
+  git clone https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh > /dev/null 2>&1
+  
+  echo 'Successfully installed oh-my-zsh'
+fi
