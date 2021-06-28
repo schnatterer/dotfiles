92f3a557558bc0bf4de6474091251197801b8c10
Johannes Schnatterer <johannes@schnatterer.info>
2021-06-28 21:06:50 +0200

    Fix completion broken by NVM

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,1 +1,4 @@
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Don't move this to completion.zsh or other completions loaded before wont work
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
