2aa01797d1e89280a8ab74a524679679842e3f41
Johannes Schnatterer <johannes@schnatterer.info>
2021-06-29 21:47:12 +0200

    Improve startup time of brew completion

--- brew/path.zsh
+++ brew/path.zsh
@@ -1,1 +1,7 @@
 [[ -s /home/linuxbrew/.linuxbrew/bin/brew ]] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+# Completion. Don't load in completion.zsh, because this needs to be added before compinit
+# https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+fi
