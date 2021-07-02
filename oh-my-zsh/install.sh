4edacbf957783fbb294af56bb3f2b3fd7f072049
Johannes Schnatterer <johannes@schnatterer.info>
2021-07-02 12:48:02 +0200

    Fix oh-my-zsh not being installed

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -6,4 +6,14 @@ if ! [ -d "${HOME}/.oh-my-zsh" ]; then
   git clone https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh > /dev/null 2>&1
   
   echo 'Successfully installed oh-my-zsh'
+  
 fi
+
+  # This belongs to powerlevel10 folder but needs to be executed after cloning omz 
+  # -> https://github.com/schnatterer/dotfiles-loader/issues/1
+  (
+    p10kfolder="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    if ! [ -d "${p10kfolder}" ]; then
+      git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${p10kfolder}"
+    fi
+  )
--- powerlevel10k/install.sh
+++
@@ -1,6 +0,0 @@
-(
-  p10kfolder="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-  if ! [ -d "${p10kfolder}" ]; then
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${p10kfolder}"
-  fi
-)
