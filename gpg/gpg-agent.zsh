d4aa5b12d89cfcaf60b6470aa8ae9939d7f6e5e4
Johannes Schnatterer <johannes@schnatterer.info>
2021-07-28 15:32:35 +0200

    Fix gpg-connect agent startup

--- gpg/gpg-agent.zsh
+++ gpg/gpg-agent.zsh
@@ -7,11 +7,11 @@
 
 # ssh over gpg
 # Launch gpg-agent
-if [[ $(command -v gpg-connect-agent >/dev/null 2>&1) ]]; then
+if command -v gpg-connect-agent >/dev/null 2>&1; then
   gpg-connect-agent /bye
   # When using SSH support, use the current TTY for passphrase prompts
   gpg-connect-agent updatestartuptty /bye > /dev/null
-  # Point the SSH_AUTH_SOCK to the one handled by gpg-agen
+  # Point the SSH_AUTH_SOCK to the one handled by gpg-agent
   
   AGENT_SOCK=$(gpgconf --list-dirs | grep agent-socket | sed 's/.*://')
   
