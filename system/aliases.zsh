7a38386134a1e68372c5bb463929347069fd40c4
Johannes Schnatterer <johannes@schnatterer.info>
2021-07-28 15:23:34 +0200

    Use duf and broot as modern replacements.
    For du and tree.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -7,7 +7,13 @@ alias json="bat -p --language='json'"
 alias "c=xclip"
 alias "v=xclip -o"
 
+# modern/faster/saner alternatives to common unix commands
 command -v exa >/dev/null 2>&1 && alias ls=exa
+if [[ -f  ~/.config/broot/launcher/bash/br ]]; then
+  source ~/.config/broot/launcher/bash/br
+  command -v br >/dev/null 2>&1 && alias tree=br
+fi
+command -v duf >/dev/null 2>&1 && alias df=duf
 
 idea() {
   /snap/intellij-idea-ultimate/current/bin/idea.sh "$*"&!
