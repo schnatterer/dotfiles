956e80dd0c62e82d0942588651037fc19c1aa7d6
Johannes Schnatterer <johannes@schnatterer.info>
2023-01-18 11:36:49 +0100

    Add regctl

--- docker/cli-tool-aliases.zsh
+++ docker/cli-tool-aliases.zsh
@@ -12,5 +12,8 @@ else
 fi
 
 #alias reg='docker run --rm -v ${HOME}:/root r.j3ss.co/reg'
+# Prefer tfenv
 #alias terraform='drrmitv ${HOME}:${HOME} --network=host -u "$(id -u):$(id -g)" -w $(pwd) hashicorp/terraform:0.12.24'
 #alias velero='docker run --rm -u $(id -u):$(id -g) -v ${HOME}:/home -eHOME=/home velero/velero'
+# e.g. regctl tag ls ghcr.io/cloudogu/helm
+alias regctl='docker run --rm ghcr.io/regclient/regctl'
