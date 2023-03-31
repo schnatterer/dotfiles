2651f03b57d919040233fa8182afbb2860819834
Johannes Schnatterer <johannes@schnatterer.info>
2023-03-31 18:07:21 +0200

    Add JetBrains Toolbox to PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:/snap/bin:${DOTFILES_ROOT}/bin:${HOME}/.local/bin:${PATH}"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:/snap/bin:${DOTFILES_ROOT}/bin:${HOME}/.local/bin:${PATH}:${HOME}/.local/share/JetBrains/Toolbox/scripts"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:${MANPATH}"
