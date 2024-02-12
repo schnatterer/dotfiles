6329124ed6e5856de38f14de915cf907839932ce
Johannes Schnatterer <johannes@schnatterer.info>
2024-02-12 16:03:19 +0100

    Enable more hidden gems in gitconfig
    My personal best of from
    https://floss.social/@b0rk@jvns.ca/111885364225550089

--- powerlevel10k/path.zsh
+++ powerlevel10k/path.zsh
@@ -4,7 +4,7 @@
 
 # Start tmux in the same process, so ctrl + d closes the window right away instead of returning to a non-tmux shell
 # If you want a terminal without tmux start a new window with NO_TMUX=1 zsh or  zsh -c "NO_TMUX=1 zsh"
-command -v tmux >/dev/null 2>&1 && if [[ "$TMUX" = "" ]] && [[ -z "$NO_TMUX" ]]; then; 
+command -v tmux >/dev/null 2>&1 && if [[ "$TMUX" = "" ]] && [[ -z "$NO_TMUX" ]]; then
   exec tmux
 fi
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
