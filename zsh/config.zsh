0514ceefaaf16d69270fdf21fe34dd4a30a6717a
Johannes Schnatterer <johannes@schnatterer.info>
2025-03-10 15:30:27 +0100

    zsh: ctrl+u like it's bash

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -2,3 +2,6 @@
 # Keep in mind that the whole thing is kept in memory!
 export HISTSIZE=10000000
 export SAVEHIST=${HISTSIZE}
+
+# Ctrl+u behavior as in bash. Opposite of ctrl+k 🙂
+bindkey \^U backward-kill-line
